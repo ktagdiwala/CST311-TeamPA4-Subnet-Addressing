51913ea52c3a8911d699eb9323af39e5bd384a13
Jorge <jav200021@gmail.com>
2024-10-18 20:27:07 -0700

    Added a CWD argument for the commands

--- certificate_generation.py
+++ certificate_generation.py
@@ -16,17 +16,17 @@ subject = (
 # Function to generate the private keys for the chat server 
 def genPrivateKey():
     print("Generating Private Keys...")
-    subprocess.run(["sudo", "openssl", "genrsa", "-out", f"{chat_server_name}-key.pem", "2048"])
+    subprocess.run(["sudo", "openssl", "genrsa", "-out", f"{chat_server_name}-key.pem", "2048"], cwd="/etc/ssl/demoCA")
 
 # Function to generate the cert signing requests for the chat server
 def genCSRs():
     print("Generating Certificate Signing Requests...")
-    subprocess.run(["sudo", "openssl", "req", "-nodes", "-new", "-config", "/etc/ssl/openssl.cnf", "-key", f"{chat_server_name}-key.pem", "-out", f"{chat_server_name}.csr", "-subj", subject])
+    subprocess.run(["sudo", "openssl", "req", "-nodes", "-new", "-config", "/etc/ssl/openssl.cnf", "-key", f"{chat_server_name}-key.pem", "-out", f"{chat_server_name}.csr", "-subj", subject], cwd="/etc/ssl/demoCA")
 
 # Function to generate the certs from the CSRs generated from genCSRs for the chat server
 def genCertFromCSRs():
     print("Generating Certificates from CSRs...")
-    subprocess.run(["sudo", "openssl", "x509", "-req", "-days", "365", "-in", f"{chat_server_name}.csr", "-CA", "cacert.pem", "-CAkey", "cakey.pem", "-CAcreateserial", "-out", f"{chat_server_name}.pem", "-passin", f"pass:{passphrase}"])
+    subprocess.run(["sudo", "openssl", "x509", "-req", "-days", "365", "-in", f"{chat_server_name}.csr", "-CA", "cacert.pem", "-CAkey", "./private/cakey.pem", "-CAcreateserial", "-out", f"{chat_server_name}.pem", "-passin", f"pass:{passphrase}"], cwd="/etc/ssl/demoCA")
 
 # Opening the file and writing the common name of the server
 with open("credentials.txt", "w") as file:
