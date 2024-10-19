4f8cfce32d66fe38a9aa57e50d09b85ae579b2fe
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-10-18 19:10:10 -0700

    got to run files without error msg

--- certificate_generation.py
+++ certificate_generation.py
@@ -26,18 +26,18 @@ def genCSRs():
 # Function to generate the certs from the CSRs generated from genCSRs for the chat server
 def genCertFromCSRs():
     print("Generating Certificates from CSRs...")
-    subprocess.run(["sudo", "openssl", "x509", "-req", "-days", "365", "-in", f"{chat_server_name}.csr", "-CA", "cacert.pem", "-CAkey", "./private/cakey.pem", "-CAcreateserial", "-out", f"{chat_server_name}.pem", "-passin", f"pass:{passphrase}"])
+    subprocess.run(["sudo", "openssl", "x509", "-req", "-days", "365", "-in", f"{chat_server_name}.csr", "-CA", "cacert.pem", "-CAkey", "cakey.pem", "-CAcreateserial", "-out", f"{chat_server_name}.pem", "-passin", f"pass:{passphrase}"])
 
-# # Opening the file and writing the common name of the server
-# with open("credentials.txt", "w") as file:
-#     file.write(chat_server_name)
+# Opening the file and writing the common name of the server
+with open("credentials.txt", "w") as file:
+    file.write(chat_server_name)
 
 # Writing into the hosts file the IP and common name of the chat server
 # Then genreating the certificates needed
 try:
     print("Getting admin privileages")
-    # with open("/etc/hosts", "a") as f:
-    #     f.write(f"10.0.2.4 {chat_server_name}")
+    with open("/etc/hosts", "a") as f:
+        f.write(f"10.0.2.4 {chat_server_name}\n")
     # subprocess.run(["sudo", "su"])
     # subprocess.run(["sudo", "echo", f"10.0.2.4 {chat_server_name}", ">>","\etc\hosts"])
     # subprocess.run(["exit"])
